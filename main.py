1e45950d1506169d9f1d70ab85adb4f36fcf24e4
Hao Wang <wh.tenghe@gmail.com>
2026-02-16 10:49:01 +1100

    doc: rename the api

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from api.pentest_results import pentest as pentest_router
+from api.get_pentest_results import pentest as pentest_router
 from api.get_token import token as demo_router
 
 app = FastAPI(title="DER Dashboard APIs Overview", tags=["connect"])
